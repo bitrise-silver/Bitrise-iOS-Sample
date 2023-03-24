0b06f90b0877b96659936fff104c16f739cc093a
Silvercast <silv ercast.nguyen@bitrise.io>
2023-03-24 20:08:10 +0100

    Add watchOS target

---
+++ BitriseTestWatch Watch App/BitriseTestWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitriseTestWatchApp.swift
+//  BitriseTestWatch Watch App
+//
+//  Created by Silvercast Nguyen on 2023-03-24.
+//
+
+import SwiftUI
+
+@main
+struct BitriseTestWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
